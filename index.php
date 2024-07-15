f4b74dfa32b473c0b1728fc44bc4e35e373f2bad
mankai-amine <medaminemankai@gmail.com>
2024-07-14 21:07:45 -0400

    structure updated
    The structure of the project is updated.
    Some modifications done for the existing files to take into account the updated structure.
    login.html is created.

--- index.php
+++ index.php
@@ -1,14 +1,35 @@
-<?php
-// Autoload dependencies
-require('vendor/autoload.php');
+<?php 
 
-// Create a new instance of the Fat-Free Framework
-$f3 = Base::instance();
-//set autoload to access files from wherever
-$f3->set('AUTOLOAD', 'controller/');
+// load the composer required libraries
+require "vendor/autoload.php";
+
+// load the framework
+$f3 = Base::instance(); 
+
+// framework to automatically load the classes within 
+$f3->set('AUTOLOAD', 'Controllers/');
+
+// framework to automatically load the templates (VIEWS) within
+$f3->set("UI", "Views/");
+
+// debugging
+$f3->set("DEBUG", 3);
 
 
+// set routes
 
-$f3->route('GET|POST /helloworld/@name', 'pages->action');
+// -> home
+$f3->route("GET @home: /", 'Pages->home');
+
+// -> contact
+$f3->route("GET @contact: /contact", 'Pages->contact');
+
+// -> contact
+$f3->route("GET @account: /account", 'Pages->account');
+
+// -> login
+$f3->route("GET @login: /login", 'Pages->login');
+//$f3->route("POST @login: /login/@name", 'Pages->loginSave');
+
+//$f3->route('GET|POST /helloworld/@name', 'pages->action');  /* How does this work ? */
 $f3->run();
-
