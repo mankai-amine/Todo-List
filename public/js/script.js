87fb301fc29b3015e3030294dfd5e8e47c7ad28f
kirillKnyaz <161050906+kirillKnyaz@users.noreply.github.com>
2024-07-19 21:38:43 -0400

    responsivity - index

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,17 @@
-
-
-
-
-
-
 $(document).ready(function(){
   let newTaskForm = $("#new");
+  let accountNav = $('#accountNav');
   newTaskForm.hide();
+  accountNav.hide();
 
   $("#addTask").click(function(){
     newTaskForm.show();
   });
+
+  $('#Account').click(function(){
+    accountNav.show();
+  });
+ 
+  console.log("js is working");
 });
 
