56094cb09a4220fd1de037a4c812316783275718
KonstKhorev <161489825+KonstKhorev@users.noreply.github.com>
2024-07-23 21:53:34 -0400

    Update Controller.php

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -12,11 +12,27 @@ class Controller{
      */
     function __construct($f3){
         $this->f3 = $f3; // f3 instance to be used
+
+        // Start the session
+        session_start();
+
+        // Flag to know if user is logged in
+        $f3 -> isSessLoggedIn = (array_key_exists("sessIsAuth", $_SESSION) && $_SESSION['sessIsAuth'] == true);
         
         // setup of page title
         $f3->set('pageTitle', '');
+        
         // setup errors variables used on several pages
         $f3->set("errors", null);
+
+        // Initialize item inputs with empty strings
+        $f3->set('item', [
+            'name' => '',
+            'email' => '',
+            'password' => '',
+            'password2' => ''
+        ]);
+
 
         // setup template
         $this->template = new Template;
@@ -37,7 +53,17 @@ class Controller{
         }
 
         $this->f3->set('pageTitle', $newTitle);
-
-
     } 
+
+    /**
+     * Redirect the user if they are not logged in
+     * @param boolean $loginFlag
+     */
+    function loginRequired($loginFlag){
+        // if the login is not true then redirect
+        if ($loginFlag != true){
+            $this->f3->reroute("@home");
+        }
+    }
+
 }
